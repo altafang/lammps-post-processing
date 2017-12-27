7a5d0921ae1e2817dc7226e65f71ccd4980956ea
Alta Fang <alta@princeton.edu>
2017-12-27 11:26:22 -0500

    handle case when thermo_style multi does not have Volume, and clean up code and comments

--- parse_lammps_log/parse_log.py
+++ parse_lammps_log/parse_log.py
@@ -4,7 +4,7 @@
 Copied and adapted some code from Pymatgen, which has a MIT License. See:
 https://github.com/materialsproject/pymatgen/blob/master/pymatgen/io/lammps/output.py
 
-Parses a LAMMPs log file.
+Class for parsing a LAMMPs log file.
 
 Alta Fang, 2017
 """
@@ -33,16 +33,18 @@ class LammpsLog:
         """
         Parse the log file for run and thermodynamic data.
         Automatically detects thermo_style and parses accordingly.
-        Sets the thermodynamic data as a dictionary with field names
-        taken from the custom thermo_style command if using thermo_style custom. 
+        Sets the thermodynamic data as a dictionary with field names taken from the 
+        custom thermo_style command if using thermo_style custom, or the column headings 
+        if using thermo_style multi or one.
+        
+        The thermo_data dictionary contains numpy arrays (not lists as in pymatgen).
         """
 
         thermo_data = []
         fixes = []
         d_build = None
-        thermo_pattern_custom = None
+        thermo_pattern = None
         thermo_multi_keys = None
-        thermo_pattern_one = None
         with open(self.log_file, 'r') as logfile:
             for line in logfile:
                 # Exclude comments
@@ -73,11 +75,12 @@ class LammpsLog:
                 fmt = re.search(r'thermo_style.+', line) 
                 if fmt and not d_build:
                     thermo_type = fmt.group().split()[1]
+                    # Set up parsing of the appropriate style
                     if thermo_type == "custom":
                         fields = fmt.group().split()[2:]
                         thermo_pattern_string = r"\s*([0-9eE\.+-]+)" + "".join(
                             [r"\s+([0-9eE\.+-]+)" for _ in range(len(fields) - 1)])
-                        thermo_pattern_custom = re.compile(thermo_pattern_string)
+                        thermo_pattern = re.compile(thermo_pattern_string)
                     elif thermo_type == "multi": 
                         thermo_multi_keys = "Step CPU TotEng KinEng Temp PotEng E_bond \
                                 E_angle E_dihed E_impro E_vdwl E_coul E_long Press \
@@ -85,22 +88,17 @@ class LammpsLog:
                         # Create dictionary of empty lists
                         thermo_data.append({key:[] for key in thermo_multi_keys})
                     else: # thermo_style one
-                        # Includes Volume if volume varies, otherwise does not
                         fields = "Step Temp E_pair E_mol TotEng Press Volume".split() 
                         thermo_pattern_string = r"\s*([0-9eE\.+-]+)" + "".join( \
                             [r"\s+([0-9eE\.+-]+)" for _ in range(len(fields) - 2)]) \
-                            + r"\s*([0-9eE\.+-]*)"
-                        thermo_pattern_one = re.compile(thermo_pattern_string)
-                # Parse custom style
-                if thermo_pattern_custom:
-                    if thermo_pattern_custom.search(line):
-                        m = thermo_pattern_custom.search(line)
-                        thermo_data.append(tuple([float(x) for x in m.groups()]))
-                elif thermo_pattern_one:
-                    if thermo_pattern_one.search(line):
-                        m = thermo_pattern_one.search(line)
+                            + r"\s*([0-9eE\.+-]*)" # Last field, Volume, may not be there
+                        thermo_pattern = re.compile(thermo_pattern_string)
+                # Parse thermo_style custom or one
+                if thermo_pattern:
+                    if thermo_pattern.search(line):
+                        m = thermo_pattern.search(line)
                         thermo_data.append(tuple([float(x) for x in m.groups() if x]))
-                # Parse multi style by appending to corresponding lists
+                # Parse thermo_style multi
                 elif thermo_multi_keys: 
                     step_re = re.search(r'Step\s+([0-9eE]+).+', line)
                     if step_re:
@@ -108,30 +106,28 @@ class LammpsLog:
                         thermo_data[-1]['Step'].append(step)
                     remaining_re = re.findall(r'\s*([a-zA-Z_]+)\s+=\s+([0-9eE\.+-]+)\s*', 
                                               line)
+                    # Append to the appropriate lists in the dictionary
                     if remaining_re:
                         for tup in remaining_re:
                             if tup[0] in thermo_data[-1].keys():
                                 thermo_data[-1][tup[0]].append(float(tup[1]))
 
         if thermo_data:
-            if isinstance(thermo_data[0], str):
-                self.thermo_data = [thermo_data]
-            elif isinstance(thermo_data[0], tuple): # thermo_style custom or one
-                # Changing return type from list to np.array, compared to pymatgen 
-                # BUGFIX ALTA: Also need to ensure that thermo_data is the right length!
+            if isinstance(thermo_data[0], tuple): # thermo_style custom or one
+                # Concatenate to ensure that thermo_data is the right length
                 try:
                     thermo_data = thermo_data[:int(self.nmdsteps/self.interval+1)]
                 except AttributeError: # Catch if nmdsteps or interval are not attributes
                     pass
-                # numpy arrays are easier to reshape, previously we used np.array with dtypes
-                # Handle case when there is no Volume in thermo_style one
+                # Exclude Volume if it's not there
                 if len(fields) != len(thermo_data[0]): 
                     fields = fields[:-1]
                 self.thermo_data = {fields[i]: np.array([thermo_data[j][i] 
                                     for j in range(len(thermo_data))])
                                     for i in range(len(fields))}
             else: # thermo_style multi
-                self.thermo_data = {k:np.array(v) for (k, v) in thermo_data[0].items()}
+                self.thermo_data = {k:np.array(v) for (k, v) in thermo_data[0].items()
+                                    if v} # Exclude Volume if it's empty
 
         self.fixes = fixes
         self.dangerous_builds = d_build
